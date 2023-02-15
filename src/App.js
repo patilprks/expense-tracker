db107c5795c5764db30e16b41d8b6c31d8ec1466
patilprks <patilprks@gmail.com>
2023-02-15 20:03:35 +0530

    added expense json

--- src/App.js
+++ src/App.js
@@ -1,31 +1,78 @@
+import React, {useState} from 'react';
 import './App.css';
-import Expense from './components/Expense';
+import Expense from './components/Expense/Expense';
+import NewExpense from './components/NewExpense/NewExpense';
+
+const DUMMY_EXPENSES = [
+  {
+    id: '1',
+    title: 'Gym Fees',
+    amount: 12000,
+    date: new Date(2020, 6, 14),
+  },
+  {
+    id: '2',
+    title: 'Text book',
+    amount: 8000,
+    date: new Date(2021, 2, 25),
+  },
+  {
+    id: '3',
+    title: 'Online Course',
+    amount: 1024,
+    date: new Date(2022, 5, 12),
+  },
+    {
+    id: '4',
+    title: 'Milk',
+    amount: 4000,
+    date: new Date(2020, 1, 25),
+  },
+  {
+    id: '5',
+    title: 'Fruits',
+    amount: 2500,
+    date: new Date(2021, 8, 10),
+  },
+  {
+    id: '6',
+    title: 'Households',
+    amount: 10000,
+    date: new Date(2022, 9, 15),
+  },
+    {
+    id: '7',
+    title: 'Servicing',
+    amount: 15000,
+    date: new Date(2020, 4, 14),
+  },
+  {
+    id: '8',
+    title: 'Insurance',
+    amount: 35000,
+    date: new Date(2021, 9, 22),
+  },
+  {
+    id: '9',
+    title: 'Yoga Class',
+    amount: 20000,
+    date: new Date(2022, 3, 4)
+  }, 
+];
+
 function App() {
 
-  const expense = [
-    {
-      id: '1',
-      title: 'Gym Fees',
-      amount: 15000,
-      date: new Date(2020, 6, 14),
-    },
-    {
-      id: '2',
-      title: 'Text book',
-      amount: 800,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: '3',
-      title: 'Online Course',
-      amount: 1024,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+ const [expense, setExpense] = useState(DUMMY_EXPENSES)
+
+  const addExpenseHandler = (NewExpenseData) => {
+    setExpense((prevExpense) =>{
+      return [NewExpenseData, ...prevExpense]
+    })
+  }
 
   return (
     <div className="App">
-      <h1 className="App__heading">Expense Tracker</h1>
+      <NewExpense onAddExpense={addExpenseHandler} />
      <Expense items={expense} />
       
     </div>
